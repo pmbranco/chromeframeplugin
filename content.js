949fcd8b24a0a3ad9e33b9f0b2708fe27d75b1d5
DaWhite <pedro.manuel.branco@gmail.com>
2018-05-26 00:45:18 +0100

    Initial commit

---
+++ content.js
@@ -0,0 +1,18 @@
+// content.js
+var height = '40px';
+var iframe = document.createElement('iframe');
+iframe.src = chrome.extension.getURL('toolbar.html');
+iframe.style.height = height;
+iframe.style.width = '100%';
+iframe.style.position = 'fixed';
+iframe.style.top = '0';
+iframe.style.left = '0';
+iframe.style.zIndex = '938089'; // Some high value
+// Etc. Add your own styles if you want to
+
+var bodyStyle = document.body.style;
+var cssTransform = 'transform' in bodyStyle ? 'transform' : 'webkitTransform';
+bodyStyle[cssTransform] = 'translateY(' + height + ')';
+document.documentElement.appendChild(iframe);
+
+
