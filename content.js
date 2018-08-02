76861048b8221edfc4e44e7898d705d14cc56f35
DaWhite <pedro.manuel.branco@gmail.com>
2018-08-02 21:51:16 +0100

    Added comments and new naming
    Commented the code
    Changed naming

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // content.js
-var height = '40px';
+var height = '60px';
 var iframe = document.createElement('iframe');
 iframe.src = chrome.extension.getURL('toolbar.html');
 iframe.style.height = height;
@@ -7,12 +7,13 @@ iframe.style.width = '100%';
 iframe.style.position = 'fixed';
 iframe.style.top = '0';
 iframe.style.left = '0';
-iframe.style.zIndex = '938089'; // Some high value
-// Etc. Add your own styles if you want to
-
+iframe.style.zIndex = '9999999'; // Some high value
+//Inject some styles
 var bodyStyle = document.body.style;
 var cssTransform = 'transform' in bodyStyle ? 'transform' : 'webkitTransform';
+//Adjust body height to inserted bar at top
 bodyStyle[cssTransform] = 'translateY(' + height + ')';
+// Add Frame to Page
 document.documentElement.appendChild(iframe);
 
 
